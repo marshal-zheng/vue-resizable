1f4e6c475fe3d2d243769518015544d9fc5d5c0d
zhq <marshal.zheng@gmaill.com>
2024-08-05 17:36:25 +0800

    chore: remove unused code in cjs.ts

--- lib/cjs.ts
+++ lib/cjs.ts
@@ -1,6 +1,5 @@
 const { Resizable, ResizableBox } = require('./ResizableBox');
 
 module.exports.Resizable = Resizable;
-// module.exports.default = Resizable;
-module.exports.ResizableBox = ResizableBox;
 
+module.exports.ResizableBox = ResizableBox;
